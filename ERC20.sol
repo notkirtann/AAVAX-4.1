8bc7e97a206d9a382ccb85e11d1bd168218f8e1e
Kirtan Nahar <119801478+notkirtann@users.noreply.github.com>
2024-08-21 22:41:18 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -9,23 +9,23 @@ contract ERC20 {
     string public symbol = "CR7";
     uint8 public decimals = 18;
 
-    event Transfer(address indexed from, address indexed to, uint value);
+		event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
 
-    function sendTokens(address recipient, uint amount) external returns (bool) {
+    function transfer(address recipient, uint amount) external returns (bool) {
         balanceOf[msg.sender] -= amount;
         balanceOf[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    function authorizeSpender(address spender, uint amount) external returns (bool) {
+    function approve(address spender, uint amount) external returns (bool) {
         allowance[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
 
-    function spendFrom(
+    function transferFrom(
         address sender,
         address recipient,
         uint amount
@@ -37,13 +37,13 @@ contract ERC20 {
         return true;
     }
 
-    function createTokens(uint amount) external {
+    function mint(uint amount) external {
         balanceOf[msg.sender] += amount;
         totalSupply += amount;
         emit Transfer(address(0), msg.sender, amount);
     }
 
-    function destroyTokens(uint amount) external {
+    function burn(uint amount) external {
         balanceOf[msg.sender] -= amount;
         totalSupply -= amount;
         emit Transfer(msg.sender, address(0), amount);
